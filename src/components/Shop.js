967142049036e9ecfb5e0eafb061b2e5cae6b3a1
Zander99900 <dipankarkarmakardk007@gmail.com>
2023-04-29 23:48:54 +0530

    Created Redux-app and action-creators & reducers

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>Buy PUMA Shoes: ₹100</h2>
+      <button type="button" className="btn btn-info mx-2">-</button>
+      Add this item to cart
+      <button type="button" className="btn btn-info mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
